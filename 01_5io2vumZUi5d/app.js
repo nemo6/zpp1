21ebbf4dc4e309a70a11172e559827c1df09fa39
nemo6 <miguel.maudet.contact@gmail.com>
2025-09-20 02:45:57 +0200

    Update app.js

--- 01_5io2vumZUi5d/app.js
+++ 01_5io2vumZUi5d/app.js
@@ -1,40 +1,64 @@
+let ignore = [
+".dropbox.cache",
+".dropbox",
+"desktop.ini",
+]
+
+const escapeRegExp = require("C:/Users/Miguel/Dropbox/E lab2/a.code/javascript/escapeRegExp_module_exp")
+
+let IGNORE_REG = new RegExp( ignore.map( escapeRegExp ).join("|") )
+
 ;( async () => {
 
 	// require("os").platform()
 	// require("os").hostname()
+	// require("os").userInfo().username
 
-	let name = require("os").hostname()
+	let name = require("os").userInfo().username
+	console.log( name )
+	// return
 
 	const transform = require(`C:/Users/${name}/Dropbox/E lab2/a.code/javascript/transform_module_exp`)
 	const _ = require(`C:/Users/${name}/AppData/Roaming/npm/node_modules/lodash`)
 	const f_server = require(`C:/Users/${name}/Dropbox/E lab2/a.code/javascript/f_server_module_exp`)
-	let f_glob = x => glob( x + "**" , { absolute:true, mark:true } )
-	const glob = require(`C:/Users/${name}/AppData/Roaming/npm/node_modules/glob`).globSync
-	let a = (await walk_folder(`C:/Users/${name}/Downloads/Nouveau dossier (6)/Dropbox/`)).at(0)
-	let b = (await walk_folder(`C:/Users/${name}/Dropbox/`)).at(0)
 
-	;( (m) => {
+	// let a = ( await walk_folder(`C:/Users/${name}/Downloads/Nouveau dossier (6)/Dropbox/`) ).at(0)
+	// let b = ( await walk_folder(`C:/Users/${name}/Dropbox/`) ).at(0)
+
+	// foo( { a } , `C:/Users/${name}/Downloads/Nouveau dossier (6)/` )
+	// foo( { b } , `C:/Users/${name}/` )
+
+	function foo( obj, rp ){
+
+		let [filename,m] = Object.entries(obj).at(0)
+
 		for( let i=0;i<m.length;i++ )
-			m[i].pathx = m[i].pathx.replace(`C:/Users/${name}/Downloads/Nouveau dossier (6)/`,"")
-	})(a)
+			m[i].pathx = m[i].pathx.replace( rp , "" )
 
-	;( (m) => {
-		for( let i=0;i<m.length;i++ )
-			m[i].pathx = m[i].pathx.replace(`C:/Users/${name}/`,"")
-	})(b)
+		require("fs").writeFileSync( `${filename}.txt` , JSON.stringify( m,null,2 ) )
 
-	// console.log( a.filter( x => x.at(-1) == "\\" ).length )
-	// console.log( a.filter( x => x.at(-1) != "\\" ).length )
-	// console.log( a.filter( x => x.type == "file" ).length )
-	// console.log( a.filter( x => x.type == "folder" ).length )
+	}
 
-	let result = _.differenceBy( a,b, "pathx").map( x => x.pathx)
+	let a = JSON.parse(require("fs").readFileSync("a.txt"))
+	let b = JSON.parse(require("fs").readFileSync("b.txt"))
+
+	let result = _.differenceBy( a,b, "hashx" ).map( x => x.pathx )
 	console.log( result.length )
 	f_server( transform( result ) , "html" )
 
+	// let result = _.differenceBy( a,b, "pathx" ).map( x => x.pathx )
+	// console.log( result.length )
+	// f_server( transform( result ) , "html" )
+
 })()
 
-async function walk_folder(dir,level=0){ // file only
+function f_hash(x){
+	let [algo,format] = ["md5","base64"]
+	let hashx = require("crypto").createHash(algo).update(x).digest(format)
+	return hashx
+}
+
+async function walk_folder( dir, level=0, count={ count:0 } ){
 
 	let list = await require("fs").promises.readdir(dir)
 
@@ -46,6 +70,8 @@ async function walk_folder(dir,level=0){ // file only
 
 		let pathx = require("path").posix.join(dir,filename)
 
+		if( (IGNORE_REG).test( pathx ) ) continue
+
 		let stats = await require("fs").promises.lstat(pathx)
 
 		if( stats.isSymbolicLink() ){
@@ -54,13 +80,27 @@ async function walk_folder(dir,level=0){ // file only
 
 		if ( stats.isFile() ){
 
-			table.push({ "pathx":pathx, "size":stats.size, "type":"file" })
+			let hashx = ""
+
+			try{
+				hashx = f_hash( await require("fs").promises.readFile(pathx) )
+			}catch(e){}
+
+			table.push({
+				"pathx":pathx,
+				"size":stats.size,
+				"type":"file",
+				"hashx" : hashx,
+			})
+
+			count.count++
+			console.log( count.count )
 
 			size += stats.size
 
 		}else if ( stats.isDirectory() ){
 
-			let [a,b] = await walk_folder(pathx,level+1)
+			let [a,b] = await walk_folder(pathx,level+1,count)
 			table.push(...a)
 			size += b
 
@@ -68,7 +108,7 @@ async function walk_folder(dir,level=0){ // file only
 
 	}
 
-	if( level != 0 )
+	// if( level != 0 )
 	table.push({ "pathx":dir, "size":size, "type":"folder" })
 
 	return [table,size]
